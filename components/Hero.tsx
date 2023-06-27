2a0569ace9ccd565932ebb6ea921778265bc4b4c
masa yamanaka <mooshkid@gmail.com>
2023-06-27 17:56:40 +0900

    up to 1hr in video

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,11 @@
+"use client";
 import Image from "next/image"
+import CustomButton from "./CustomButton"
 
 const Hero = () => {
+
+  const handleScroll = () => {}
+
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
@@ -8,6 +13,20 @@ const Hero = () => {
           Find, book, or rent a car -- quickly and easily!
         </h1>
         <p className="hero__subtitle">Steamline your care rental experience with our effortless booking process.</p>
+
+        <CustomButton 
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+        </div>
+        
+        <div className="hero__image-overlay"></div>
       </div>
     </div>
   )
--- components/index.ts
+++ components/index.ts
@@ -1,4 +1,18 @@
 import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
 
 
-export {Hero}
+export {
+  Hero,
+  CustomButton,
+  Navbar,
+  Footer,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+}
